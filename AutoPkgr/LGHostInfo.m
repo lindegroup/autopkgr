aef1567a52373d5a0b9fc77f4736e692a2b7b1d3
futureimperfect <james@everythingisgray.com>
2014-06-28 23:42:04 -0700

    Now correctly reporting whether git/autopkg are installed. Git can also be installed by prompting for CLI tools install.

--- AutoPkgr/LGHostInfo.m
+++ AutoPkgr/LGHostInfo.m
@@ -27,4 +27,30 @@
     return userAtHostName;
 }
 
+- (BOOL)gitInstalled
+{
+    NSArray *knownGitPaths = [[NSArray alloc] initWithObjects:@"/usr/bin/git", @"/usr/local/bin/git", @"/opt/boxen/homebrew/git", nil];
+
+    for (NSString *path in knownGitPaths) {
+        if ([[NSFileManager defaultManager] isExecutableFileAtPath:path]) {
+            return YES;
+        }
+    }
+
+    return NO;
+}
+
+- (BOOL)autoPkgInstalled
+{
+    NSArray *knownAutoPkgPaths = [[NSArray alloc] initWithObjects:@"/usr/local/bin/autopkg", @"/usr/bin/autopkg", nil];
+
+    for (NSString *path in knownAutoPkgPaths) {
+        if ([[NSFileManager defaultManager] isExecutableFileAtPath:path]) {
+            return YES;
+        }
+    }
+
+    return NO;
+}
+
 @end
