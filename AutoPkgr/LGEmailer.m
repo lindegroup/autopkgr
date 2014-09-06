777907f63a2ce7dfee0749a0c7cf9e9ca8a310e2
Eldon Ahrold <eldon.ahrold@gmail.com>
2014-09-06 02:26:41 -0500

    Add New Emailer Method

--- AutoPkgr/LGEmailer.m
+++ AutoPkgr/LGEmailer.m
@@ -29,16 +29,16 @@
 - (void)sendEmailNotification:(NSString *)subject message:(NSString *)message
 {
     LGDefaults *defaults = [[LGDefaults alloc] init];
-
-    BOOL TLS = [defaults SMTPTLSEnabled];
-
+    
+    BOOL TLS = defaults.SMTPTLSEnabled;
+    
     MCOSMTPSession *smtpSession = [[MCOSMTPSession alloc] init];
-    smtpSession.hostname = [defaults objectForKey:kLGSMTPServer];
-    smtpSession.port = (int)[defaults integerForKey:kLGSMTPPort];
-    smtpSession.username = [defaults objectForKey:kLGSMTPUsername];
-
+    smtpSession.hostname = defaults.SMTPServer;
+    smtpSession.port = (int)defaults.SMTPPort;
+    smtpSession.username = defaults.SMTPUsername;
+    
     if (TLS) {
-        NSLog(@"SSL/TLS is enabled for %@.", [defaults objectForKey:kLGSMTPServer]);
+        NSLog(@"SSL/TLS is enabled for %@.", defaults.SMTPServer);
         // If the SMTP port is 465, use MCOConnectionTypeTLS.
         // Otherwise use MCOConnectionTypeStartTLS.
         if (smtpSession.port == 465) {
@@ -47,20 +47,20 @@
             smtpSession.connectionType = MCOConnectionTypeStartTLS;
         }
     } else {
-        NSLog(@"SSL/TLS is _not_ enabled for %@.", [defaults objectForKey:kLGSMTPServer]);
+        NSLog(@"SSL/TLS is _not_ enabled for %@.", defaults.SMTPServer);
         smtpSession.connectionType = MCOConnectionTypeClear;
     }
-
+    
     // Retrieve the SMTP password from the default
     // keychain if it exists
     NSError *error = nil;
-    NSString *smtpUsernameString = [defaults objectForKey:kLGSMTPUsername];
-
+    NSString *smtpUsernameString = defaults.SMTPUsername;
+    
     if (smtpUsernameString) {
         NSString *password = [SSKeychain passwordForService:kLGApplicationName
                                                     account:smtpUsernameString
                                                       error:&error];
-
+        
         if ([error code] == SSKeychainErrorNotFound) {
             NSLog(@"Keychain item not found for account %@.", smtpSession.username);
         } else if([error code] == SSKeychainErrorNoPassword) {
@@ -75,38 +75,37 @@
             }
         }
     }
-
+    
     MCOMessageBuilder * builder = [[MCOMessageBuilder alloc] init];
-
     [[builder header] setFrom:[MCOAddress addressWithDisplayName:@"AutoPkgr Notification"
-                                                         mailbox:[defaults SMTPFrom]]];
-
+                                                         mailbox:defaults.SMTPFrom]];
+    
     NSMutableArray *to = [[NSMutableArray alloc] init];
-    for (NSString *toAddress in [defaults SMTPTo]) {
+    for (NSString *toAddress in defaults.SMTPTo) {
         if (![toAddress isEqual:@""]) {
             MCOAddress *newAddress = [MCOAddress addressWithMailbox:toAddress];
             [to addObject:newAddress];
         }
     }
-
+    NSString *fullSubject = [NSString stringWithFormat:@"%@ On %@",subject,[[NSHost currentHost] name]];
     [[builder header] setTo:to];
-    [[builder header] setSubject:subject];
+    [[builder header] setSubject:fullSubject];
     [builder setHTMLBody:message];
     NSData * rfc822Data = [builder data];
-
+    
     MCOSMTPSendOperation *sendOperation = [smtpSession sendOperationWithData:rfc822Data];
     [sendOperation start:^(NSError *error) {
         NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
         NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithDictionary:@{kLGNotificationUserInfoSubject:subject,
                                                                                         kLGNotificationUserInfoMessage:message}];
-
+        
         if (error) {
             NSLog(@"%@ Error sending email:%@", smtpSession.username, error);
             [userInfo setObject:error forKey:kLGNotificationUserInfoError];
         } else {
             NSLog(@"%@ Successfully sent email!", smtpSession.username);
         }
-
+        
         [center postNotificationName:kLGNotificationEmailSent
                               object:self
                             userInfo:[NSDictionary dictionaryWithDictionary:userInfo]];
@@ -121,7 +120,68 @@
     NSString *message = @"This is a test notification from <strong>AutoPkgr</strong>.";
     // Send the email
     [self sendEmailNotification:subject message:message];
+    
+}
 
+- (void)sendEmailForReport:(NSDictionary *)report error:(NSError *)error
+{
+    // Get arrays of new downloads/packages from the plist
+    NSMutableString *message;
+    NSString *subject;
+    NSArray *newDownloads;
+    NSArray *newPackages;
+    
+    if (report) {
+        newDownloads = [report objectForKey:@"new_downloads"];
+        newPackages = [report objectForKey:@"new_packages"];
+    }
+    
+    if ([newDownloads count]) {
+        message = [[NSMutableString alloc] init];
+        NSLog(@"New stuff was downloaded.");
+        
+        // Create the subject string
+        subject = [NSString stringWithFormat:@"[%@] New Software Avaliable For Testing",kLGApplicationName];
+        
+        // Append the the message string with report
+        [message appendFormat:@"The following software is now available for testing:<br />"];
+        
+        for (NSString *path in newDownloads) {
+            // Get just the application name from the path in the new_downloads dict
+            NSString *app = [[path lastPathComponent] stringByDeletingPathExtension];
+            
+            // Write the app to the string
+            [message appendFormat:@"<br /><strong>%@</strong>: ",app];
+            
+            // The default version is undetected, override later
+            NSString *version = @"Version Undetected";
+            for (NSDictionary *dct in newPackages) {
+                NSString *pkgPath = [dct objectForKey:@"pkg_path"];
+                if ([pkgPath rangeOfString:app options:NSCaseInsensitiveSearch].location != NSNotFound && dct[@"version"]) {
+                    version = dct[@"version"];
+                    break;
+                }
+            }
+            [message appendFormat:@"%@",version];
+        }
+    } else {
+        DLog(@"Nothing new was downloaded.");
+    }
+    
+    if (error) {
+        if(!message){
+            message = [[NSMutableString alloc] init];
+        }
+        
+        if (!subject){
+            subject = [NSString stringWithFormat:@"[%@] Error Occured While Running AutoPkg",kLGApplicationName];
+        }
+        [message appendFormat:@"<strong>The following error occured:</strong><br />  %@<br />  %@",error.localizedDescription,error.localizedRecoverySuggestion];
+    }
+    
+    if (message) {
+        [self sendEmailNotification:subject message:message];
+    }
 }
 
 @end
