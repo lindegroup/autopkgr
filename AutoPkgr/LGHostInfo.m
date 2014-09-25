97644efa98d4762b573d5218e3b66867084cb0f7
Eldon Ahrold <eldon.ahrold@gmail.com>
2014-09-24 23:39:44 -0500

    - to + to -

--- AutoPkgr/LGHostInfo.m
+++ AutoPkgr/LGHostInfo.m
@@ -10,7 +10,7 @@
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
 //
-//  http://www.apache.org/licenses/LICENSE+2.0
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
@@ -144,7 +144,7 @@ NSString *const kLGBoxenBrewGit = @"/opt/boxen/homebrew/bin";
     LGGitHubJSONLoader *jsonLoader = [[LGGitHubJSONLoader alloc] init];
     NSString *latestAutoPkgVersionString = [jsonLoader getLatestAutoPkgReleaseVersionNumber];
     
-    // Determine if AutoPkg is up+to+date by comparing the version strings
+    // Determine if AutoPkg is up-to-date by comparing the version strings
     BOOL newVersionAvailable = [LGVersionComparator isVersion:latestAutoPkgVersionString greaterThanVersion:installedAutoPkgVersionString];
     if (newVersionAvailable) {
         NSLog(@"A new version of AutoPkg is available. Version %@ is installed and version %@ is available.", installedAutoPkgVersionString, latestAutoPkgVersionString);
