5d4b173ec5635201c9640db95ed7e735e75646fe
Eldon Ahrold <eldon.ahrold@gmail.com>
2015-06-27 09:27:58 -0500

    Initial commit to mac patch integration.

---
+++ AutoPkgr/View-Controllers/LGMacPatchIntegrationView.h
@@ -0,0 +1,13 @@
+//
+//  LGMacPatchIntegrationView.h
+//  AutoPkgr
+//
+//  Created by Eldon on 6/26/15.
+//  Copyright (c) 2015 The Linde Group, Inc. All rights reserved.
+//
+
+#import "LGBaseIntegrationViewController.h"
+
+@interface LGMacPatchIntegrationView : LGBaseIntegrationViewController
+
+@end
