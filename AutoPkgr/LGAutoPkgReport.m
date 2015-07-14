a487e0bcc0bc6f63922217eaa590b191a51b9023
Elliot Jordan <elliot@elliotjordan.com>
2015-07-14 09:48:19 -0700

    Reverted colon on email subject.

--- AutoPkgr/LGAutoPkgReport.m
+++ AutoPkgr/LGAutoPkgReport.m
@@ -118,7 +118,7 @@ NSString *const fallback_reportCSS = @"<style type='text/css'>*{font-family:'Hel
 - (NSString *)emailSubjectString
 {
     if ([_reportDictionary[kReportKeySummaryResults] count] > 0) {
-        return quick_formatString(NSLocalizedString(@"New software available for testing:", nil));
+        return quick_formatString(NSLocalizedString(@"New software available for testing", nil));
     } else if ([_reportDictionary[kReportKeyFailures] count] > 0) {
         return quick_formatString(NSLocalizedString(@"Failures occurred while running AutoPkg", nil));
     } else if (self.error) {
