428c1dd6b43869d685e258e9287889cb4c7b3a17
Eldon Ahrold <eldon.ahrold@gmail.com>
2016-05-13 22:49:15 -0500

    Bind buttons, and add post install actions to VirusTotalAnalyzer.

--- AutoPkgr/Views-Controllers-XIB/Integration Views/LGVirusTotalAnalyzerIntegrationView.m
+++ AutoPkgr/Views-Controllers-XIB/Integration Views/LGVirusTotalAnalyzerIntegrationView.m
@@ -30,6 +30,7 @@
 @property (weak) IBOutlet NSTextField *VirusTotalSleepSecondsTF;
 
 @property (strong) LGVirusTotalAnalyzerDefaults *defaults;
+@property BOOL  customTokenEnabled;
 
 @end
 
@@ -52,42 +53,61 @@
     // If we're using our own API key, this should be enabled and inherit whatever value is in VIRUSTOTAL_API_KEY.
     _VirusTotalApiKeyTF.enabled = NO;
 
-    _VirusTotalAutoSubmitMaxSizeTF.delegate = self;
-
-    // If VIRUSTOTAL_AUTO_SUBMIT is set to NO, this field should be disabled.
-    // If VIRUSTOTAL_AUTO_SUBMIT is set to YES, this should be enabled and inherit whatever value is in VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE.
-    _VirusTotalAutoSubmitMaxSizeTF.enabled = NO;
 
     // If there is no VIRUSTOTAL_SLEEP_SECONDS preference, this defaults to 15.
     _VirusTotalSleepSecondsTF.delegate = self;
-}
-
-- (void)controlTextDidChange:(NSNotification *)notification {
-    NSString *string = [notification.object stringValue];
-
-    // VIRUSTOTAL_API_KEY
-    if ([notification.object isEqualTo:_VirusTotalApiKeyTF]) {
-        _defaults.VIRUSTOTAL_API_KEY = string;
+    if(_defaults.VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE != 0){
+        _VirusTotalSleepSecondsTF.integerValue = _defaults.VIRUSTOTAL_SLEEP_SECONDS;
     }
 
+    // Set up the button states.
+    _VirusTotalAlwaysReportBT.state = _defaults.VIRUSTOTAL_ALWAYS_REPORT;
+    _VirusTotalAutoSubmitBT.state = _defaults.VIRUSTOTAL_AUTO_SUBMIT;
+
+    // If VIRUSTOTAL_AUTO_SUBMIT is set to NO, this field should be disabled.
+    // If VIRUSTOTAL_AUTO_SUBMIT is set to YES, this should be enabled and inherit whatever value is in VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE.
+    _VirusTotalAutoSubmitMaxSizeTF.enabled = _VirusTotalAlwaysReportBT.state;
+    _VirusTotalAutoSubmitMaxSizeTF.delegate = self;
+    if(_VirusTotalAlwaysReportBT.state){
+        _VirusTotalAutoSubmitMaxSizeTF.integerValue = _defaults.VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE;
+    }
+
+}
+
+- (IBAction)tokenTypeChanged:(NSMatrix *)matrix {
+    if(matrix.selectedRow == 0){
+        _defaults.VIRUSTOTAL_API_KEY = nil;
+    }
+}
+
+- (IBAction)checkBoxChanged:(NSButton *)button{
     // VIRUSTOTAL_ALWAYS_REPORT
-    if ([notification.object isEqualTo:_VirusTotalAlwaysReportBT]) {
-        _defaults.VIRUSTOTAL_ALWAYS_REPORT = NO;
+    if ([button isEqualTo:_VirusTotalAlwaysReportBT]) {
+        _defaults.VIRUSTOTAL_ALWAYS_REPORT = button.state;
     }
 
     // VIRUSTOTAL_AUTO_SUBMIT
-    if ([notification.object isEqualTo:_VirusTotalAutoSubmitBT]) {
-        _defaults.VIRUSTOTAL_AUTO_SUBMIT = NO;
+    if ([button isEqualTo:_VirusTotalAutoSubmitBT]) {
+        _defaults.VIRUSTOTAL_AUTO_SUBMIT = button.state;
+        _VirusTotalAutoSubmitMaxSizeTF.enabled = button.state;
+    }
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification {
+
+    // VIRUSTOTAL_API_KEY
+    if ([notification.object isEqualTo:_VirusTotalApiKeyTF]) {
+        _defaults.VIRUSTOTAL_API_KEY = [notification.object stringValue];
     }
 
     // VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE
     if ([notification.object isEqualTo:_VirusTotalAutoSubmitMaxSizeTF]) {
-        _defaults.VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE = 419430400;
+        _defaults.VIRUSTOTAL_AUTO_SUBMIT_MAX_SIZE = [notification.object integerValue];
     }
 
     // VIRUSTOTAL_SLEEP_SECONDS
     if ([notification.object isEqualTo:_VirusTotalSleepSecondsTF]) {
-        _defaults.VIRUSTOTAL_SLEEP_SECONDS = 15;
+        _defaults.VIRUSTOTAL_SLEEP_SECONDS = [notification.object integerValue];
     }
 }
 @end
